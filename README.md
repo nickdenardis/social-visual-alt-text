f1db4cee3603230afacac1825bc349e95f277aa0
Nick DeNardis <nick.denardis@gmail.com>
2021-06-13 07:34:16 -0400

    Initial proof of concept

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# social-visual-alt-text
-Browser extension to visually show image alt text on social
+# Browser extension to visually show alt text of social images
+
+This extension is designed for individuals who are interested in viewing the alt text applied to images without the need to inspect each image or use a screen reader.
+
+This is helpful to see what alt text is applied (if any) to an image which may provide additional context to the contence of the image, what a screen reader user would hear and if additional context should be added if you are planning to retweet a post.
+
+The visual text added to the page is hiddent from screen readers to ensure the text is not read to the user twice.
+
+## Current sites
+
+- [x] Twitter images
+- [x] Twitter GIFs
+- [ ] Twitter video captions
+- [ ] Facebook
+- [ ] Instagram
+
+## Contributing
+
+1. Please open an issue before submitting a pull request
+2. Fork the `nickdenardis/social-visual-alt-text` repository
+3. Make your changes on the fork
+4. Submit a PR to the `nickdenardis/social-visual-alt-text` `main` branch
+
+
+## Licensing
+
+Social visual alt is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ contentScript.js
@@ -0,0 +1,91 @@
+var insertAlt = function () {
+  const timelineImages = document.querySelectorAll(
+    'main div[data-testid="primaryColumn"] img[src^="https://pbs.twimg.com/media/"]'
+  );
+
+  const timelineGifs = document.querySelectorAll(
+    'main div[data-testid="primaryColumn"] video[src^="https://video.twimg.com/tweet_video/"]'
+  );
+
+  timelineGifs.forEach(function (userGif) {
+    if (userGif.getAttribute("data-altdisplayed") !== "true") {
+      console.log(userGif.getAttribute("aria-label"));
+      let gifLink =
+        userGif.parentElement.parentElement.parentElement.parentElement
+          .parentElement.parentElement.parentElement.parentElement.parentElement
+          .parentElement;
+
+      const altText = document.createElement("div");
+      altText.setAttribute("aria-hidden", "true");
+
+      if (
+        !userGif.getAttribute("aria-label") ||
+        userGif.getAttribute("aria-label") == "Embedded video"
+      ) {
+        altText.style.backgroundColor = "red";
+        altText.style.height = "12px";
+      } else {
+        altText.style.color = "white";
+        altText.style.backgroundColor = "blue";
+        altText.style.fontSize = "18px";
+        altText.style.padding = "4px 8px";
+        altText.style.fontFamily =
+          'Arial, "Helvetica Neue", Helvetica, sans-serif';
+        altText.textContent = userGif.getAttribute("aria-label");
+      }
+
+      if (gifLink) {
+        gifLink.append(altText);
+      }
+
+      userGif.setAttribute("data-altdisplayed", "true");
+    }
+  });
+
+  timelineImages.forEach(function (userImage) {
+    if (userImage.getAttribute("data-altdisplayed") !== "true") {
+      let imageLink = userImage.closest("a");
+
+      // If there are multiple images
+      if (imageLink) {
+        const imageCount = imageLink.parentElement.querySelectorAll("a");
+        if (imageCount.length > 1) {
+          imageLink =
+            imageLink.parentElement.parentElement.parentElement.parentElement
+              .parentElement;
+        }
+      }
+
+      const altText = document.createElement("div");
+      altText.setAttribute("aria-hidden", "true");
+
+      if (
+        !userImage.getAttribute("alt") ||
+        userImage.getAttribute("alt") == "Image"
+      ) {
+        altText.style.backgroundColor = "red";
+        altText.style.height = "12px";
+      } else {
+        altText.style.color = "white";
+        altText.style.backgroundColor = "blue";
+        altText.style.fontSize = "18px";
+        altText.style.padding = "4px 8px";
+        altText.style.fontFamily =
+          'Arial, "Helvetica Neue", Helvetica, sans-serif';
+        altText.textContent = userImage.getAttribute("alt");
+      }
+
+      if (imageLink) {
+        imageLink.append(altText);
+      }
+    }
+    userImage.setAttribute("data-altdisplayed", "true");
+  });
+};
+
+var twitterLoop = function twitterLoop() {
+  insertAlt();
+  setTimeout(twitterLoop, 500);
+};
+
+twitterLoop();
