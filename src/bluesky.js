06fb9f51c9c74b25092adf81adf73c324665e8df
MellowKyler <dewerdk@gmail.com>
2025-05-28 20:19:48 -0500

    add bluesky video support

--- src/bluesky.js
+++ src/bluesky.js
@@ -13,6 +13,13 @@ let insertAlt = function () {
           )
         : [];
 
+    // Videos
+    const timelineVideos = options.blueskyImages
+        ? document.querySelectorAll(
+              'main div[aria-label="Embedded video player"] figure'
+          )
+        : [];
+
     timelineImages.forEach(function (userImage) {
         if (userImage.getAttribute("data-altdisplayed") !== "true") {
             // Where to put the alt text in the DOM
@@ -91,6 +98,42 @@ let insertAlt = function () {
             }
 
             // Keep track of the images with alt displayed
+            userImage.setAttribute("data-altdisplayed", "true");
+        }
+    });
+
+    timelineVideos.forEach(function (userImage) {
+        if (userImage.getAttribute("data-altdisplayed") !== "true") { 
+            // Where to put the alt text in the DOM
+            let imageLink =
+                userImage.parentElement.parentElement.parentElement
+                    .parentElement.parentElement.parentElement;
+
+            // Container for visible text
+            const altText = document.createElement("div");
+            altText.setAttribute("aria-hidden", "true");
+
+            // Determine if the video has alt text on it
+            const figcaption = userImage.querySelector('figcaption');
+            if (!figcaption) {
+                altText.style.backgroundColor = options.colorNoAlt;
+                altText.style.height = "12px";
+            } else {
+                altText.style.color = options.colorAltText;
+                altText.style.backgroundColor = options.colorAltBg;
+                altText.style.fontSize = "18px";
+                altText.style.padding = "4px 8px";
+                altText.style.fontFamily =
+                    'Arial, "Helvetica Neue", Helvetica, sans-serif';
+                altText.insertAdjacentHTML('beforeend', newlineToBr(figcaption.textContent));
+            }
+
+            // Add the element to the DOM
+            if (imageLink) {
+                imageLink.append(altText);
+            }
+
+            // Keep track of the videos with alt displayed
             userImage.setAttribute("data-altdisplayed", "true");
         }
     });
